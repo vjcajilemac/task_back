05faed3d590e18abc947e344ad5b697b3869e7d9
vjcajilemac <vjcajilema@gmail.com>
2023-12-28 12:00:18 -0500

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConstants } from '../db.constant';
+import { TasksModule } from './tasks/tasks.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: dbConstants.password,
+      database: dbConstants.name,
+      synchronize: true,
+
+      autoLoadEntities: true,
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
